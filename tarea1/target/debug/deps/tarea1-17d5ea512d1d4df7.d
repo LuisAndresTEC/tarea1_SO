fd5973bc0df703d400d17265b9550785a6b1bc79
LuisAndresTEC <lrojasmurillo7@estudiantec.cr>
2022-08-09 21:30:56 -0600

    commit inicial

---
+++ tarea1/target/debug/deps/tarea1-17d5ea512d1d4df7.d
@@ -0,0 +1,5 @@
+/home/luis/Documentos/Sistemas operativos/tarea1_SO/tarea1/target/debug/deps/tarea1-17d5ea512d1d4df7.rmeta: src/main.rs
+
+/home/luis/Documentos/Sistemas operativos/tarea1_SO/tarea1/target/debug/deps/tarea1-17d5ea512d1d4df7.d: src/main.rs
+
+src/main.rs:
---
+++ tarea1/target/debug/deps/tarea1-62a8b330a4317054.d
@@ -0,0 +1,5 @@
+/home/luis/Documentos/Sistemas operativos/tarea1_SO/tarea1/target/debug/deps/tarea1-62a8b330a4317054.rmeta: src/main.rs
+
+/home/luis/Documentos/Sistemas operativos/tarea1_SO/tarea1/target/debug/deps/tarea1-62a8b330a4317054.d: src/main.rs
+
+src/main.rs:
