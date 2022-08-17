9ef1c88980dd45a5df9f92f3854d71c42107189e
LuisAndresTEC <lrojasmurillo7@estudiantec.cr>
2022-08-16 18:02:10 -0600

    codigo final 1.0

--- tarea1/src/main.rs
+++ tarea1/src/main.rs
@@ -1,5 +1,5 @@
-use std::ops::Add;
 use std::{io, ops::RangeInclusive};
+use std::io::{stdin, stdout, Read, Write};
 mod system_call_names;
 
 use linux_personality::personality;
@@ -25,12 +25,12 @@ fn fragmentador(opcion: String) -> Vec<String>{
 
 fn entrada_texto() -> String{
     let mut entrada = String::new();
-    print!("Ingrese la sentencia para el rastreador: ");
+    println!("Ingrese la sentencia para el rastreador: ");
     io::stdin().read_line(&mut entrada).unwrap();
     return entrada;
 }
 
-fn run_tracee(opcion:String) {
+fn ejecuta_binario(opcion:String) {
     ptrace::traceme().unwrap();
     personality(linux_personality::ADDR_NO_RANDOMIZE).unwrap();
     Command::new(opcion).exec();
@@ -45,28 +45,26 @@ fn menu_tracker_v(binario: String) {
     //duplica el proceso
     match unsafe { fork() } {
         Ok(ForkResult::Child) => {
-            run_tracee(binario);
+            ejecuta_binario(binario);
         }
 
         Ok(ForkResult::Parent { child }) => {
-            rastreador_v(child);
+            reps_counter_hash(rastreador_v(child));
         }
-
         Err(err) => {
             panic!("[main] fork() failed: {}", err);
         }
     }
 }
-
 fn menu_tracker_V(binario: String){
     //duplica el proceso
     match unsafe { fork() } {
         Ok(ForkResult::Child) => {
-            run_tracee(binario);
+            ejecuta_binario(binario);
         }
 
         Ok(ForkResult::Parent { child }) => {
-            rastreador_V(child);
+            reps_counter_hash(rastreador_V(child));
         }
 
         Err(err) => {
@@ -76,10 +74,16 @@ fn menu_tracker_V(binario: String){
     
 }
 
+fn presiona_tecla(){
+    let mut entrada = String::new();
+    print!("Presione alguna tecla para continuar: ");
+    io::stdin().read_line(&mut entrada).unwrap();
+}
+
 fn rastreador_V(child: Pid)-> Vec<String> {
+    let mut calls_repo:Vec<String> = Vec::new();
     loop {
         wait().unwrap();
-        //let mut calls_repo:Vec<String, String> = Vec::new();
         match ptrace::getregs(child) {
 
             Ok(x) => print!(
@@ -90,39 +94,102 @@ fn rastreador_V(child: Pid)-> Vec<String> {
                 Err(_) => break,
 
         };
-        match ptrace::syscall(child, None) {
-            Ok(_) => continue,
-            Err(_) => break,
-        }
-    }
-    return Vec::new();
-}
-
-
-fn rastreador_v(child: Pid)-> Vec<String> {
-    loop {
-        wait().unwrap();
-        //let mut calls_repo:Vec<String> = Vec::new();
         match ptrace::getregs(child) {
 
-            Ok(x) => println!(
-                "{:?} {:?}\n",
-                system_call_names::SYSTEM_CALL_NAMES[(x.orig_rax) as usize],
-                x               
-            ),
-                Err(_) => break,
+            Ok(x) =>
+            calls_repo.push((system_call_names::SYSTEM_CALL_NAMES[(x.orig_rax) as usize]).to_string()),
+            Err(_) => break,
+        }
+        presiona_tecla();
+        match ptrace::syscall(child, None) {
+            Ok(_) => continue,
+            Err(_) => break,
+        }
+        
+        
+    }
+    return calls_repo;
+}
 
+fn rastreador_v(child: Pid)-> Vec<String> {
+    let mut calls_repo:Vec<String> = Vec::new();
+    loop {
+        wait().unwrap();
+        match ptrace::getregs(child) {
+
+            Ok(x) =>
+                println!("{:?} {:?}\n",system_call_names::SYSTEM_CALL_NAMES[(x.orig_rax) as usize],x),
+                Err(_) => break,
+        
         };
+        match ptrace::getregs(child) {
+
+            Ok(x) =>
+            calls_repo.push((system_call_names::SYSTEM_CALL_NAMES[(x.orig_rax) as usize]).to_string()),
+            Err(_) => break,
+        }
         match ptrace::syscall(child, None) {
             Ok(_) => continue,
             Err(_) => break,
         }
     }
-    return Vec::new();
+    return calls_repo;
+}
+
+fn reps_counter_hash(calls_repo: Vec<String>){
+    //crea un hashmap para contar las repeticiones de las llamadas
+    let mut calls_counter: Vec<(String, i128)> = Vec::new();
+    let mut calls_copied: Vec<String> = Vec::new();
+    //recorre el arreglo de llamadas
+    for i in calls_repo.iter(){
+        //verifica si el hashmap contiene la llamada
+        if (reps_counter_hash_verifier(i.to_string(), calls_copied.to_vec())) {
+            //si contiene la llamada.
+        }else{
+            //si no contiene la llamada.
+            
+            let mut count:i128 = reps_counter_hash_aux(i.to_string(), calls_repo.to_vec());
+            calls_copied.push(i.to_string());
+            calls_counter.push((i.to_string(), count));
+           
+        }
+        
+    }
+    //imprime el hashmap
+    reps_counter_hash_printer(calls_counter);
+}
+
+fn reps_counter_hash_verifier(i: String, calls_copied: Vec<String>)-> bool{
+    
+    for j in calls_copied.iter(){
+        
+        if i == j.to_string(){
+
+            return true;
+        }
+    }
+    return false;
 }
 
 
+fn reps_counter_hash_aux(call:String, calls_repo: Vec<String>)->i128{
+   
+    let mut counter:i128 = 0;
+    for i in calls_repo{
+        if i == call{
+            counter += 1;
+        }
+    }
+    
+    return counter;
+}
 
+fn reps_counter_hash_printer(calls_counter: Vec<(String, i128)>){
+    for i in calls_counter{
+        
+        println!("Llamada: {}, Cantidad: {}", i.0 , i.1);
+    }
+}
 
 fn main() {
     let entrada: String = entrada_texto();
